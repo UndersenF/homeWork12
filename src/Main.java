41041fd144c3c0944f218adb5dd248780245a9e4
UndersenF <begintolife@mail.ru>
2024-06-11 11:05:31 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Author firstAuthor = new Author("Johana", "Rowling");
+        Author secoundAuthor = new Author("Irvin", "Yalom");
+
+        Book firstBook = new Book("Harry Potter", firstAuthor, 2000);
+        Book secondBook = new Book("When Nietzsche Wept", secoundAuthor, 1992);
+
+        System.out.println("Год публикации до изменения - " + secondBook.getPublishingYear());
+        secondBook.setPublishingYear(1995);
+        System.out.println("Год публикации после - " + secondBook.getPublishingYear());
     }
 }
